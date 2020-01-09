3aef63f6eca2c8bdf0fe2b1f94942b706ae7ca7c
SwayUser <swayuser@inbox.lv>
2020-01-09 15:38:13 +0100

    Update width

--- deleteDiscordMessages.js
+++ deleteDiscordMessages.js
@@ -3,7 +3,7 @@
 (function () {
     let stop;
     let popup;
-    popup = window.open('', '', `top=0,left=${screen.width-800},width=829,height=${screen.height}`);
+    popup = window.open('', '', `top=0,left=${screen.width-800},width=830,height=${screen.height}`);
     if (!popup) return console.error('Popup blocked! Please allow popups and try again.');
     popup.document.write(/*html*/`<!DOCTYPE html>
     <html><head><meta charset='utf-8'><title>Delete Discord Messages</title><base target="_blank">
